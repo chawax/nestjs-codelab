cd5155714b87fc530e7983baa779e33995f9c63e
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2022-10-03 17:26:58 +0200

    step1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { BookingModule } from './booking/booking.module';
-import { PlanetModule } from './planet/planet.module';
-import { StarshipModule } from './starship/starship.module';
-
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: configService.get('SQL_MEMORY_DB_SHARED'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: true,
-        } as TypeOrmModuleOptions;
-      },
-    }),
-    PlanetModule,
-    StarshipModule,
-    BookingModule,
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
