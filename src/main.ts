66d340aa2cad457eff2989c52951981f7b96a3ad
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2022-10-03 17:26:58 +0200

    step1

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,8 @@
-import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { BearerGuard } from './security/guards/bearer.guard';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get<ConfigService>(ConfigService);
-
-  // SECURITY
-  app.useGlobalGuards(new BearerGuard(configService));
-
-  // VERSIONNING
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
-
-  //------- IN & OUT
-  // Enables global behaviors on incoming DTO
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // Only expose attributes wille be accepted on incoming DTO
-      transform: true, // Automatically converts attributes from incoming DTO when possible
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
-
-  // Enables global behaviors on outgoing entities
-  // For examples, @Exclude decorators will be processed
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-  //-------- SWAGGER
-  const config = new DocumentBuilder()
-    .setTitle('Form Earth to Moon API')
-    .setDescription('A codelab to discover NestJs and more')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   await app.listen(3000);
 }
 bootstrap();
