c08a4dba55dc0ab4ac47722bada1ab0d67d55a25
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2022-10-05 16:06:55 +0200

    SWAGGER

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // VERSIONNING
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  //-------- SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('Form Earth to Moon API')
+    .setDescription('A codelab to discover NestJs and more')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
