c8f7ca70c3ff2bc5ed4c100a9a1a6371141ab511
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2022-10-05 16:07:57 +0200

    Repository usage in findAll

--- src/planet/planet.service.ts
+++ src/planet/planet.service.ts
@@ -1,53 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/repository/Repository';
 import { CreatePlanetDto } from './dto/create-planet.dto';
 import { UpdatePlanetDto } from './dto/update-planet.dto';
 import { Planet } from './entities/planet.entity';
 
 @Injectable()
 export class PlanetService {
+  constructor(
+    @InjectRepository(Planet)
+    private readonly planetRepository: Repository<Planet>,
+  ) {}
+
   create(createPlanetDto: CreatePlanetDto) {
     return 'This action adds a new planet';
   }
 
-  findAll(): Planet[] {
-    const planetsJSON = [
-      {
-        name: 'Lune',
-        distanceToEarth: 384400,
-      },
-      {
-        name: 'Venus',
-        distanceToEarth: 41400000,
-      },
-      {
-        name: 'Mars',
-        distanceToEarth: 78340000,
-      },
-      {
-        name: 'Mercure',
-        distanceToEarth: 91690000,
-      },
-      {
-        name: 'Jupiter',
-        distanceToEarth: 628730000,
-      },
-      {
-        name: 'Saturne',
-        distanceToEarth: 1275000000,
-      },
-      {
-        name: 'Uranus',
-        distanceToEarth: 2723950000,
-      },
-      {
-        name: 'Neptune',
-        distanceToEarth: 4351400000,
-      },
-    ];
-
-    const planets: Planet[] = Object.assign(new Array<Planet>(), planetsJSON);
-
-    return planets;
+  findAll(): Promise<Planet[]> {
+    return this.planetRepository.find();
   }
 
   findOne(id: number) {
--- src/starship/starship.service.ts
+++ src/starship/starship.service.ts
@@ -1,36 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateStarshipDto } from './dto/create-starship.dto';
 import { UpdateStarshipDto } from './dto/update-starship.dto';
 import { Starship } from './entities/starship.entity';
 
 @Injectable()
 export class StarshipService {
+  constructor(
+    @InjectRepository(Starship)
+    private readonly starshipRepository: Repository<Starship>,
+  ) {}
+
   create(createStarshipDto: CreateStarshipDto) {
     return 'This action adds a new starship';
   }
 
-  findAll(): Starship[] {
-    const starshipsJSON = [
-      {
-        name: 'Apollo',
-        speed: 39000,
-        kilometerPrice: 10000,
-      },
-      {
-        name: 'SpaceX Starship',
-        speed: 27000,
-        kilometerPrice: 250000,
-      },
-      {
-        name: 'Sonde Parker',
-        speed: 532000,
-        kilometerPrice: 50000,
-      },
-    ];
-
-    const starships: Starship[] = Object.assign(new Array<Starship>(), starshipsJSON);
-
-    return starships;
+  findAll(): Promise<Starship[]> {
+    return this.starshipRepository.find();
   }
 
   findOne(id: number) {
