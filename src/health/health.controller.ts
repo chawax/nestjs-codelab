22ea78bc6d1b399090f9372233cf4d7d8fbb4789
Olivier DANGREAUX <olivier.dangreaux@gmail.com>
2022-10-05 16:06:55 +0200

    /health route

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  @Get()
+  check(): string {
+    return 'Everything is OK';
+  }
+}
